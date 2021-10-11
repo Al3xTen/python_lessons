c9cd051c9805bf0abda2cab03b1ed7dc6daeefac
“John <johndoe@example.com>
2021-10-11 18:56:54 +0500

    adding first python lesson

---
+++ qalab.py
@@ -0,0 +1,65 @@
+# print('hi')
+# message='Sasha'
+# print(type(message.upper()))
+# message=5
+# # print(type(message))
+# number=5.5
+# print(int('5'))
+# print(type(5))
+#
+# temp_c=36.6
+# temp_f=temp_c*1.8+32
+# print(temp_f)
+
+# print(100*'$')
+# print('$'+98*' '+'$')
+# print('$'+42*' '+message.upper()+51*' '+'$')
+# print('$'+98*' '+'$')
+# print(100*'$')
+
+# first_name='alex'
+# last_name='ten'
+# age=26
+# print(last_name+'\n\t'+first_name+'\t Age='+str(age))
+
+# num1='2'
+# num2='4'
+# print(num1+num2)
+
+# name = tuple('sasha')
+# print(name)
+# print(type(name))
+
+# d = dict(first = 'ss',second = 'ssss')
+# print(d)
+
+# a = 4
+# b = 3
+# x = a ** b
+# print(x)
+
+# a=5+3j
+# print(a.real)
+# print(a.imag)
+
+# a = 2002
+# # print(hex(a))
+# # print(oct(a))
+# # print(bin(a))
+
+# a = '3'
+# b = 2.3
+# print(int(a)+b)
+
+# import math
+# print(math.ceil(2.2))
+# print(math.floor(2.2))
+# print(math.fabs(-2.2))
+# print(math.factorial(4))
+# print(math.pow(2,3))
+# print(math.sqrt(2))
+
+# a, b = 0, 1
+# while a<10:
+#     print(a)
+#     a, b = b, a+b
